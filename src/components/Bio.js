385c5d60ea44a72bd7e25039dbf7b2326ac1b89a
Matti Petrelius <matti.petrelius@gmail.com>
2019-04-20 14:57:09 +0300

    Fix bio image alt text

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -27,7 +27,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Matti Petrelius`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
