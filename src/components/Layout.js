a6b19216b4be10686d886f9305990161470f1da2
Matti Petrelius <matti.petrelius@gmail.com>
2019-04-21 19:28:20 +0300

    Fix margins for headers

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -59,7 +59,7 @@ class Layout extends React.Component {
           style={{
             fontFamily: 'Montserrat, sans-serif',
             marginTop: 0,
-            marginBottom: rhythm(-1),
+            marginBottom: rhythm(1),
           }}
         >
           <Link
