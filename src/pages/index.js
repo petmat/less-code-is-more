b0a417035551955d108ee627d964726578822880
Matti Petrelius <matti.petrelius@gmail.com>
2020-07-17 20:22:44 +0300

    fix visit counter

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,7 +91,6 @@ export const pageQuery = graphql`
         title
         description
         siteUrl
-        visitCount
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
